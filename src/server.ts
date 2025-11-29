0e60b682180f076de2a1cb5ca60ceb64459360fc
Christos Nounis <christosno1984@gmail.com>
2025-11-29 17:50:53 +0200

    global middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,25 @@
 import express from "express";
+import { isTest } from "../env.ts";
+
 import authRoutes from "./routes/authRoutes.ts";
 import walkRoutes from "./routes/walkRoutes.ts";
 import userRoutes from "./routes/userRoutes.ts";
 
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
 const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan("dev", {
+    skip: () => isTest(),
+  })
+);
 
 app.get("/health", (req, res) => {
   res.status(200).json({
