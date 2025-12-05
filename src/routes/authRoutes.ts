9b9e8914a89dd5461267706a1838e5a152b8d386
Christos Nounis <christosno1984@gmail.com>
2025-12-05 13:46:06 +0200

    apply the login controller to login endpoint

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,16 +1,21 @@
 import { Router } from "express";
-import { register } from "../controllers/authController.ts";
+import { login, register } from "../controllers/authController.ts";
 import { validateBody } from "../middleware/validation.ts";
 import { userInsertSchema } from "../db/schema.ts";
+import z from "zod";
 
 const router = Router();
 
 router.post("/register", validateBody(userInsertSchema), register);
 
-router.post("/login", (req, res) => {
-  res.status(201).json({
-    message: "Login successful",
-  });
+const loginSchema = z.object({
+  email: z.email("Invalid email format"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .max(255, "Password must be less than 255 characters long"),
 });
+
+router.post("/login", validateBody(loginSchema), login);
 
 export default router;
