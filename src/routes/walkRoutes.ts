a7fea4ea512cc2184bfbc59b8bf75b6db86027b5
Christos Nounis <christosno1984@gmail.com>
2025-12-07 11:07:35 +0200

    protect user and walks routes

--- src/routes/walkRoutes.ts
+++ src/routes/walkRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { validateBody, validateParams } from "../middleware/validation.ts";
 import { z } from "zod";
+import { authenticateToken } from "../middleware/auth.ts";
 
 const createWalkSchema = z.object({
   name: z.string().min(1),
@@ -15,6 +16,8 @@ const updateWalkParamsSchema = z.object({
 });
 
 const router = Router();
+
+router.use(authenticateToken);
 
 router.get("/", (req, res) => {
   res.status(200).json({
